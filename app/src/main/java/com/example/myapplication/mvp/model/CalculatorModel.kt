d6398e9e1a043d149fb2d5af1f9c4d2158973136
Braian Quinteros <braian.quinteros@globant.com>
2019-12-09 12:00:50 -0300

    changed open classes

--- app/src/main/java/com/example/myapplication/mvp/model/CalculatorModel.kt
+++ app/src/main/java/com/example/myapplication/mvp/model/CalculatorModel.kt
@@ -3,9 +3,22 @@ package com.example.myapplication.mvp.model
 import com.example.myapplication.utils.EMPTY_STRING
 import com.example.myapplication.utils.ZERO_FLOAT_RESULT
 
-open class CalculatorModel {
+class CalculatorModel {
     var operatorOne: String = EMPTY_STRING
     var operatorTwo: String = EMPTY_STRING
     var operand: String = EMPTY_STRING
     var result: Float = ZERO_FLOAT_RESULT
+
+    fun resetAll() {
+        operatorOne = EMPTY_STRING
+        operatorTwo = EMPTY_STRING
+        operand = EMPTY_STRING
+        result = ZERO_FLOAT_RESULT
+    }
+
+    fun resetVisor() {
+        operatorOne = EMPTY_STRING
+        operatorTwo = EMPTY_STRING
+        operand = EMPTY_STRING
+    }
 }
